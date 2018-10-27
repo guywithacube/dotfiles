84fa4aae9687d483225b28bfe19d21e42b71c673
Jason Hsu <23387405+guywithacube@users.noreply.github.com>
2018-10-27 02:46:27 -0700

    Added verbose descriptions of actions

--- install.sh
+++ install.sh
@@ -5,19 +5,24 @@ DOTFILES_DIR=~/dotfiles
 #==========
 # Delete pre-existing dot files and folders
 #==========
-rm -rf ~/.bash_profile > /dev/null 2>&1
-rm -rf ~/.bash_aliases > /dev/null 2>&1
-rm -rf ~/.bashrc > /dev/null 2>&1
-rm -rf ~/.tmux.conf > /dev/null 2>&1
-rm -rf ~/.vimrc > /dev/null 2>&1
-rm -rf ~/.vim > /dev/null 2>&1
+# > /dev/null 2>&1
+printf "Removing any existing directories\n"
+rm -rfv ~/.bash_profile
+rm -rfv ~/.bash_aliases
+rm -rfv ~/.bashrc
+rm -rfv ~/.tmux.conf
+rm -rfv ~/.vimrc
+rm -rfv ~/.vim
+printf "\n"
 
 #==========
 # Create symlinks in home
 #==========
-ln -sf $DOTFILES_DIR/bash/bash_profile ~/.bash_profile
-ln -sf $DOTFILES_DIR/bash/bash_aliases ~/.bash_aliases
-ln -sf $DOTFILES_DIR/bash/bashrc ~/.bashrc
-ln -sf $DOTFILES_DIR/tmux/tmux.conf ~/.tmux.conf
-ln -sf $DOTFILES_DIR/vim/vimrc ~/.vimrc
-ln -sf $DOTFILES_DIR/vim ~/.vim
+printf "Creating symbolic links\n"
+ln -sfv $DOTFILES_DIR/bash/bash_profile ~/.bash_profile
+ln -sfv $DOTFILES_DIR/bash/bash_aliases ~/.bash_aliases
+ln -sfv $DOTFILES_DIR/bash/bashrc ~/.bashrc
+ln -sfv $DOTFILES_DIR/tmux/tmux.conf ~/.tmux.conf
+ln -sfv $DOTFILES_DIR/vim/vimrc ~/.vimrc
+ln -sfv $DOTFILES_DIR/vim ~/.vim
+printf "\n"
